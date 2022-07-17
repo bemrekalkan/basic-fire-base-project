ee817851617a799441e40b42d1d6b4282fa07d49
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-17 23:19:29 +0300

    firebase installed

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDt0vTVc_EY2atyEUnGxdVXeZLpsiWLLiA",
+  authDomain: "people-201cb.firebaseapp.com",
+  projectId: "people-201cb",
+  storageBucket: "people-201cb.appspot.com",
+  messagingSenderId: "299528096851",
+  appId: "1:299528096851:web:df19367d8e300920aecaa6",
+};
