e055713f0b20d17977bba39971914aa0cc0ff90a
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-18 09:07:45 +0300

    some changes

--- src/Person.js
+++ src/Person.js
@@ -1,11 +1,17 @@
 import React from "react";
+import db from "./firebase";
+import "./Person.css";
 
 const Person = (id, name, surname) => {
+  const deletePerson = () => {
+    db.collection("people").doc(id).delete();
+  };
   return (
     <div className="person">
       <h3>
         {name} {surname}
       </h3>
+      <button onClick={deletePerson}>Delete</button>
     </div>
   );
 };
